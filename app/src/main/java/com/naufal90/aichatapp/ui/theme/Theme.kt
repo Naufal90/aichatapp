bfe6e05a0c4a558fc2dd4e8e24020878eedca3ee
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-05-22 17:57:51 +0700

    update app/src/main/java/com/naufal90/aichatapp/ui/theme/Theme.kt

---
+++ app/src/main/java/com/naufal90/aichatapp/ui/theme/Theme.kt
@@ -0,0 +1,27 @@
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+)
+
+@Composable
+fun AIChatAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    CompositionLocalProvider(LocalSpacing provides Dimensions()) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
+}
